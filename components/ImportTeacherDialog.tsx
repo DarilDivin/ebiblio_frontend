425f6f7caf670e4893fefb24154fb0159d676055
DarilDivin <darkout2004@gmail.com>
2024-07-24 10:59:25 +0100

    Corrrection de la taille de l'input de l'import des enseignants

--- components/ImportTeacherDialog.tsx
+++ components/ImportTeacherDialog.tsx
@@ -95,7 +95,7 @@ const ImportTeacherDialog = () => {
               <Form {...form}>
                 <form
                   onSubmit={form.handleSubmit(onSubmit)}
-                  className="w-2/3 space-y-6"
+                  className="w-full space-y-6"
                 >
                   <FormField
                     control={form.control}
